3c5306edabd870790f6775e79df5f51758497635
Oskar2000 <s25020@pjwstk.edu.pl>
2023-03-10 19:04:36 +0100

    Usuniecie petli

--- main.py
+++ main.py
@@ -4,11 +4,24 @@ import requests as requests
 
 if __name__ == '__main__':
  pageurl = input("Podaj stronę internetową: ")
- for x in range(3):
-  date = input("Podaj date: ")
-  url = "http://archive.org/wayback/available?url="+pageurl+"&timestamp="+str(date)
-  response = requests.get(url)
-  d = response.json()
-  page = d["archived_snapshots"]["closest"]["url"]
-  webbrowser.open(page)
+ date = input("Podaj date: ")
+ url = "http://archive.org/wayback/available?url="+pageurl+"&timestamp="+str(date)
+ response = requests.get(url)
+ d = response.json()
+ page = d["archived_snapshots"]["closest"]["url"]
+ webbrowser.open(page)
+ pageurl = input("Podaj stronę internetową: ")
+ date = input("Podaj date: ")
+ url = "http://archive.org/wayback/available?url="+pageurl+"&timestamp="+str(date)
+ response = requests.get(url)
+ d = response.json()
+ page = d["archived_snapshots"]["closest"]["url"]
+ webbrowser.open(page)
+ pageurl = input("Podaj stronę internetową: ")
+ date = input("Podaj date: ")
+ url = "http://archive.org/wayback/available?url="+pageurl+"&timestamp="+str(date)
+ response = requests.get(url)
+ d = response.json()
+ page = d["archived_snapshots"]["closest"]["url"]
+ webbrowser.open(page)
 print("Program zakonczyl petle")
